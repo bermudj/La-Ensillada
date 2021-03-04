22a7328acb1e7c0d26573b3c7dae1cf5949da883
bermudj <79755857+bermudj@users.noreply.github.com>
2021-03-04 16:38:20 +0000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    while True:
+        try:
+            data = input("Enter: ")
+        except EOFError:
+            break
+
+        s.sendall(data.encode())
+        data = s.recv(1024)
+        print('Got reply', repr(data.decode("utf-8")))
